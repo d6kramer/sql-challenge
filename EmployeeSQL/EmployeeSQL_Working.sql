c8110a4d217cb4371ef97bb5cdbcab5858f2cfb1
Dan Kramer <dan.r.kramer@gmail.com>
2024-02-23 23:19:44 -0800

    DA #2 complete.

--- EmployeeSQL/EmployeeSQL_Working.sql
+++ EmployeeSQL/EmployeeSQL_Working.sql
@@ -40,9 +40,12 @@ CREATE TABLE dept_emp (
 );
 
 CREATE VIEW data_analysis_01 AS
-SELECT e.emp_no, e.last_name, e.first_name, e.sex, s.salary
-FROM employees e
-JOIN salaries s using (emp_no);
+	SELECT e.emp_no, e.last_name, e.first_name, e.sex, s.salary
+	FROM employees e
+	JOIN salaries s using (emp_no);
 
-SELECT * from data_analysis_01;
-
+CREATE VIEW data_analysis_02 AS
+	SELECT first_name, last_name, hire_date
+	FROM employees
+	WHERE hire_date >= '01/01/1986'
+	AND hire_date <= '12/31/1986';
--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # sql-challenge
 Module 9 Challenge Repo
+
+
+-- Solution to search between a range of dates in a SQL Query (settled on the greater than/less than solution from given answers):
+
+https://stackoverflow.com/questions/11985536/sql-like-statement-on-a-datetime-type
